83e9ef808e3c050edd13b86a7d948e3afa80de79
Nabeel Akbar <studiosnabeel@gmail.com>
2022-11-09 13:40:47 +0500

    webpack config file modify

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
     }),
   ],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
 };
